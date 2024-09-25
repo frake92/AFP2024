8a843158da7d5563c1cf2445e6186cb6d89b45db
Batki Roland <batkiroli@gmail.com>
2024-09-25 19:01:45 +0200

    Létrehoztam a login.php-t
    Maga a belépés részét kezelő kód.

---
+++ docs/Projekt/login.php
@@ -0,0 +1,27 @@
+<?php
+    include("connection.php");
+
+    session_start();
+
+    if(isset($_POST['submit'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($result);
+
+        if($count==1) {
+            $_SESSION['logged_in'] = true;
+            echo "<script>alert('Sikeres belépés!'); window.location.href='marketplace.php';</script>";
+        } else {
+            echo '<script>
+                window.location.href = "loginpage.php";
+                alert("Belépés sikertelen.")
+                </script>';
+        }
+
+    }
+
+?>
