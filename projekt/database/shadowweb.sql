94b8e136c4b7703271edd901c00688a8bd476116
FLajos-YC2F1U <farkas.lajos1911@gmail.com>
2024-10-06 21:30:48 +0200

    shadowweb.sql módosítása. guns tábla feltöltése fegyverekkel, és hozzáadva plusz egy felhasználó user role-ként.

--- projekt/database/shadowweb.sql
+++ projekt/database/shadowweb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Okt 01. 12:34
+-- Létrehozás ideje: 2024. Okt 06. 21:15
 -- Kiszolgáló verziója: 10.4.32-MariaDB
--- PHP verzió: 8.0.30
+-- PHP verzió: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,6 +34,23 @@ CREATE TABLE `guns` (
   `picture_path` varchar(255) NOT NULL,
   `description` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `guns`
+--
+
+INSERT INTO `guns` (`id`, `gun_name`, `price`, `picture_path`, `description`) VALUES
+(1, 'M1911', 20000.00, '/AFP2024/projekt/web/images/1911.jpg', 'Az M1911 egy klasszikus félautomata pisztoly, amelyet John Browning tervezett, és 1911-ben rendszeresítették az Egyesült Államok hadseregében. A fegyver több mint 70 évig volt szolgálatban, és számos konfliktusban használták, ami kiemeli megbízhatóságát és időtállóságát. Az M1911 .45 ACP (Automatic Colt Pistol) kalibert használ, amely nagy átütőerőt és megállító erőt biztosít.\r\n\r\nA pisztoly fémből készült váza és egyszerű, mégis rendkívül hatékony működési mechanizmusa miatt népszerű volt mind katonai, mind civil felhasználók körében. Kiemelkedő jellemzője az egyeslövetű elsütőszerkezet, amely pontos lövéseket tesz lehetővé, miközben a masszív felépítése biztos fogást és stabilitást nyújt lövés közben.\r\n\r\nAz airsoft változatoknál az M1911 replikák hűen tükrözik a fegyver eredeti megjelenését és működését. Klasszikus dizájnja és megbízhatósága miatt az airsoft játékosok körében is közkedvelt, különösen azok számára, akik a hagyományos stílusú, jól kezelhető pisztolyokat kedvelik.'),
+(2, 'Desert Eagle .50AE', 35000.00, '/AFP2024/projekt/web/images/desert.jpg', 'A Desert Eagle .50 AE egy legendás félautomata pisztoly, amelyet elsősorban a hatalmas ereje és ikonikus megjelenése miatt ismernek. A Magnum Research által fejlesztett fegyver a .50 Action Express (AE) kalibert használja, ami az egyik legerősebb lőszer, amit pisztolyokhoz terveztek. Ez a masszív kaliber rendkívüli átütőerőt és torkolati energiát biztosít, így a Desert Eagle nemcsak vadászatnál, hanem sportlövészetnél is kiváló választás.'),
+(3, 'MP5', 55000.00, '/AFP2024/projekt/web/images/mp5.jpg', ''),
+(4, 'MP7', 60000.00, '/AFP2024/projekt/web/images/mp7.jpg', ''),
+(5, 'AK-47', 80000.00, '/AFP2024/projekt/web/images/ak47.jpg', ''),
+(6, 'M4', 90000.00, '/AFP2024/projekt/web/images/M4.jpg', ''),
+(7, 'Dragunov', 120000.00, '/AFP2024/projekt/web/images/Dragunov.jpg', ''),
+(8, 'Kar98K', 140000.00, '/AFP2024/projekt/web/images/kar98.png', ''),
+(9, 'M249', 200000.00, '/AFP2024/projekt/web/images/M249.jpg', ''),
+(10, 'PKM', 225000.00, '/AFP2024/projekt/web/images/pkm.jpg\r\n', ''),
+(11, 'Ballistic Shield', 30000.00, '/AFP2024/projekt/web/images/Ballistic.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -69,7 +86,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `birthdate`, `role`, `created_at`) VALUES
-(1, 'admin123', 'admin', 'admin@admin.com', '1900-01-01', 'admin', '2024-09-27 18:02:20');
+(1, 'admin123', 'admin', 'admin@admin.com', '1900-01-01', 'admin', '2024-09-27 18:02:20'),
+(2, 'Lajos', 'asd', 'asd@vmi.com', '2002-02-03', 'user', '2024-10-06 17:20:10');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -103,7 +121,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `guns`
 --
 ALTER TABLE `guns`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT a táblához `inventories`
@@ -115,7 +133,7 @@ ALTER TABLE `inventories`
 -- AUTO_INCREMENT a táblához `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Megkötések a kiírt táblákhoz
