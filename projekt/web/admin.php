b08f55f9e2be48c2127c596ca83aa1cb5e2973bd
Kakuk Márton GUOV01 <145665813+Marcizjeh@users.noreply.github.com>
2024-10-07 14:37:55 +0200

    admin.php megírásának elkezdése.

---
+++ projekt/web/admin.php
@@ -0,0 +1,12 @@
+<?php
+    require_once './API/connection.php';
+    session_start();
+
+    if(isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        
+        
+    }
+?>
