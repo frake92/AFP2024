9b52b0f0c187814c4e1f1559678ad96993a82190
Kakuk Márton GUOV01 <145665813+Marcizjeh@users.noreply.github.com>
2024-10-07 14:41:49 +0200

    admin.php navigáció elkészitése

--- projekt/web/admin.php
+++ projekt/web/admin.php
@@ -9,5 +9,15 @@
         $sql = "SELECT * FROM users WHERE username = ? AND password = ? AND role = 'admin'";        
         $result = $stmt->get_result();
         
+        if($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $_SESSION['admin_id'] = $row['id'];  
+            header("Location: adminpage.php");   
+            exit();
+        } else {
+            echo '<script>
+                window.location.href = "admin.php";
+                alert("Belépés sikertelen.")
+                </script>';
     }
 ?>
